e952875ebe31836f5cc2cbb3a448218eac46d2b6
Vinepine <vincentcalle@hotmail.com>
2025-12-05 00:04:11 +0100

    station route

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const users = require('./src/routes/users.js');
+const stations = require('./src/routes/stations.js');
 const app = express();
 
 const port = process.env.PORT || 3001;
@@ -8,6 +9,9 @@ app.get('/v1/users', users.getUsers);
 
 app.get('/v1/users/:id', users.specificUser);
 
+app.get('/v1/stations', stations.getStations);
+
+app.get('/v1/stations/:id', stations.getOneStation);
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
