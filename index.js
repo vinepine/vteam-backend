522467b030a2fff7bd0d7f61a44fc4ecac921657
Vinepine <vincentcalle@hotmail.com>
2025-12-05 01:19:21 +0100

    fixade filstrukturen för en renare index.js

--- index.js
+++ index.js
@@ -1,26 +1,17 @@
 const express = require('express')
-const users = require('./src/routes/users.js');
-const scooters = require('./src/routes/scooters.js');
-const stations = require('./src/routes/stations.js');
+
+const scooterRoutes = require('./src/routes/scooters.js');
+const stationRoutes = require('./src/routes/stations.js');
+const userRoutes = require('./src/routes/users.js');
+
 const app = express();
 
 const port = process.env.PORT || 3001;
 
-app.get('/v1/users', users.getUsers);
+app.use(scooterRoutes);
+app.use(stationRoutes);
+app.use(userRoutes);
 
-app.get('/v1/users/:id', users.specificUser);
-
-app.get('/v1/bike', scooters.getScooters);
-
-app.get('/v1/bike/:id', scooters.getOneScooter);
-
-app.get('/v1/bike/available', scooters.getAvailable);
-
-app.get('/v1/bike/:id/:available', scooters.updateAvailable);
-
-app.get('/v1/stations', stations.getStations);
-
-app.get('/v1/stations/:id', stations.getOneStation);
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
